5d258884bf6c5a031c47d174a40d77c41649d3c9
Tony Young <tony@rfw.name>
2012-07-16 22:55:02 +1200

    first commit.

---
+++ docs.md
@@ -0,0 +1,48 @@
+<a href="https://github.com/rfw/owldoc"><img style="position: fixed; top: 0; right: 0; border: 0;" src="https://s3.amazonaws.com/github/ribbons/forkme_right_darkblue_121621.png" alt="Fork me on GitHub"></a>
+
+# Owldoc
+
+![Owldoc](logo.png)
+
+_A JavaScript prose documentation generator._
+
+Owldoc is a tool for generating prose documentation using Node.js, in the style of
+Sphinx. It accepts a Markdown file and an EJS template file, and proceeds to
+generate a documentation file with them.
+
+Owldoc is freely distributable under the terms of the MIT license.
+
+## Basic Usage
+
+    $ owldoc --template=docs.ejs.html docs.md > docs.html
+
+And hey presto, your documentation is now generated!
+
+## Rendering
+
+The template is supplied with the following context variables:
+
+ * `title`
+
+   The text of the first heading.
+
+ * `content`
+
+   Contains the rendered Markdown.
+
+ * `toc`
+
+   Contains a table of contents tree. It has three attributes: `id` which
+   contains the anchor to a section, `children` which contain all items
+   underneath this level, and `title` for the text of the section. If `id` is
+   `null`, then this section doesn't actually exist, and the item is there just
+   to act as a placeholder.
+
+During rendering, all headings are given `id` attributes for anchors. These
+anchors are resolved in order — for instance, the anchor to this section is
+[`#Rendering`](#Rendering). If another section was named "Rendering" after this
+section, then the anchor to that section will be named `#Rendering-2`.
+
+If you want to override the anchor text, simply surround the section of the
+title you want to make the anchor with `<span class="heading"></span>`.
+
---
+++ owldoc.js
@@ -0,0 +1,104 @@
+#!/usr/bin/env node
+
+var md = require("node-markdown").Markdown,
+    cheerio = require("cheerio"),
+    ejs = require("ejs"),
+    fs = require("fs"),
+    argv = require("optimist")
+        .usage("Generate documentation with a Markdown file\n" +
+               "Usage: $0 FILENAME")
+        .demand(1, 1)
+        .demand('t')
+        .alias('t', 'template')
+        .describe('t', 'EJS template to use')
+        .argv;
+
+var template = ejs.compile(fs.readFileSync(argv.t).toString());
+
+var slugify = function(x) {
+    return x
+        .replace(/[^\w\s-]/g, '')
+        .replace(/[-\s]+/g, '-')
+        .trim();
+};
+
+argv._.forEach(function(fn) {
+    // Render our content.
+    var content = fs.readFileSync(fn).toString();
+
+    // Create a Cheerio context to work with.
+    var $ = cheerio.load(md(content));
+
+    // Create a ToC structure.
+    var toc = {
+        children: []
+    };
+
+    // Keep a breadcrumb structure of the ToC nodes.
+    var tocBreadcrumbs = [toc];
+
+    // Keep track of the slugs we have.
+    var slugs = {};
+
+    // The title of the document.
+    var title = null;
+
+    // Walk through all the heading elements.
+    $("h1, h2, h3, h4, h5, h6").each(function(i, node) {
+        var level = parseInt(node.name.substring(1));
+
+        // Pop ToC nodes off the breadcrumbs until we're at the right place.
+        while(tocBreadcrumbs.length > level) {
+            tocBreadcrumbs.pop();
+        }
+
+        // Push ToC nodes onto the breadcrumbs until we're at the right place.
+        while(tocBreadcrumbs.length < level) {
+            var tocNode = {
+                children: []
+            };
+
+            tocBreadcrumbs[tocBreadcrumbs.length - 1].children.push(tocNode);
+            tocBreadcrumbs.push(tocNode);
+        }
+
+        var text = $(node).find(".heading").length ?
+            $(node).find(".heading").text() :
+            $(node).text();
+
+        if(title === null) {
+            title = text;
+        }
+
+        var slugBase = slugify(text);
+        var slug = slugBase;
+
+        var i = 1;
+
+        while(Object.prototype.hasOwnProperty.call(slugs, slug)) {
+            slug = slugBase + '-' + i++;
+        }
+
+        // Set the node's ID.
+        $(node).attr('id', slug);
+
+        //
+        slugs[slug] = true;
+
+        var tocNode = {
+            id: slug,
+            title: text,
+            children: []
+        };
+
+        tocBreadcrumbs[tocBreadcrumbs.length - 1].children.push(tocNode);
+        tocBreadcrumbs.push(tocNode);
+    });
+
+    process.stdout.write(template({
+        toc: toc,
+        title: title,
+        content: $.html()
+    }));
+});
+
